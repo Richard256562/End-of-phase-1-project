b1f65794e64f750f9468652874549e8f43efdf39
RichardOkongo <rirachard19@gmail.com>
2022-12-16 21:57:30 +0300

    made changes to js file

--- Index.js
+++ Index.js
@@ -25,6 +25,9 @@ let weather = {
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
  }
 };

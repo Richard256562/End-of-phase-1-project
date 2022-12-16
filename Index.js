ead20a432f4f3f8b59f7f0a99d168e9af437ebc3
RichardOkongo <rirachard19@gmail.com>
2022-12-16 21:52:25 +0300

    query selector for description

--- Index.js
+++ Index.js
@@ -19,7 +19,7 @@ let weather = {
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
-
+      document.querySelector(".description").innerText = description;
  }
 };
 
